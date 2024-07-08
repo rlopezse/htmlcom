2868287a1e08a329563a00fd07dbd5e5a1cd5709
Ronald Lopez <ronaldlopez@Mac-mini-de-Ronald.local>
2024-07-07 23:29:41 -0400

    edit: first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Test</title>
+    <meta name="description" content="PHP Test">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <h1>PHP Test</h1>
+    <?php echo '<p>Hello World</p>'; ?>
+    <script src="js/functions.js"></script>
+</body>
+</html>
