6a0639f9410b4efd888c66326f6d0a737457971b
Ronald Lopez <ronaldlopez@Mac-mini-de-Ronald.local>
2024-07-14 17:24:42 -0400

    add: analytics

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-9R5WH6HP4K"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-9R5WH6HP4K');
+    </script>
     <meta charset="UTF-8">
     <title>PHP Test</title>
     <meta name="description" content="PHP Test">
