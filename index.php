c65a1749e7b611bd49302101648bfcda80f197ad
Ronald Lopez <ronaldlopez@Mac-mini-de-Ronald.local>
2024-07-14 17:43:21 -0400

    fix: github action e index

--- index.php
+++ index.php
@@ -11,13 +11,13 @@
       gtag('config', 'G-9R5WH6HP4K');
     </script>
     <meta charset="UTF-8">
-    <title>PHP Test</title>
-    <meta name="description" content="PHP Test">
+    <title>Ronald's Portfolio</title>
+    <meta name="description" content="Ronald's Portfolio">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
-<body>
+<body
+    <?php echo "hola mundo"; ?>
     <h1>PHP Test</h1>
-    <?php echo '<p>Hello World</p>'; ?>
     <script src="js/functions.js"></script>
 </body>
 </html>
