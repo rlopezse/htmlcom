767b4b15258caa18b854fd885288ec2a373cad67
Ronald Lopez <ronaldlopez@Mac-mini-de-Ronald.local>
2024-07-15 18:37:06 -0400

    edit: ajustes home

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <!-- Google tag (gtag.js) -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-9R5WH6HP4K"></script>
     <script>
@@ -9,15 +11,17 @@
       gtag('js', new Date());
       gtag('config', 'G-9R5WH6HP4K');
     </script>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
     <meta charset="UTF-8">
     <title>Ronald's Portfolio</title>
     <meta name="description" content="Ronald's Portfolio">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/bundle.min.css">
+    <link rel="stylesheet" href="css/bundle.min.css?v=1">
 </head>
 <body>
     <div class="rz-container">
-        <h1>Ronald Portfolio</h1>
+        <h1>Hi, I'm Ronald</h1>
+        <p>Front-End, Vimmer and Pixel Artist.</p>
     </div>
     <script src="js/functions.js"></script>
 </body>
